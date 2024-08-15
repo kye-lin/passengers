dd9428a9ec82899fb7f816e67b90e835809792cb
kye-lin <kyesqlin@gmail.com>
2024-08-15 12:55:35 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+let count = 0;
+
+// store the count-el element in a countEl variable
+let countEl = getElementById("count-el");
+
+function increment() {
+	count = count + 1;
+	// set countEl's innerText to the count
+	countEl.innerText = count;
+};
+
+// Create a function, save(), which logs out the count when it's called 
